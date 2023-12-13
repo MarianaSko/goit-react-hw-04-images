21988b022bdefaaa6320b9c20b02672977f8617e
MarianaSko <maryana.skorynovych@gmail.com>
2023-12-13 19:20:48 +0200

    v-1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,15 +3,14 @@ import s from '../../main.module.css';
 
 export const Modal = ({ src, alt, onModalClose }) => {
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        onModalClose();
+      }
+    };
     document.addEventListener('keydown', handleKeyDown);
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, []);
-
-  const handleKeyDown = e => {
-    if (e.key === 'Escape') {
-      onModalClose();
-    }
-  };
+  }, [onModalClose]);
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
